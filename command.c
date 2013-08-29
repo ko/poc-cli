b7b02d046a2c9c5bd802ca8d56c9f8bfaed2a4ef
Ken Ko <ken.ko@harmonicinc.com>
2013-08-29 16:30:02 -0700

    Output for the commands given.

--- command.c
+++ command.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "cli.h"
 #include "command.h"
 
@@ -10,29 +11,47 @@ struct cli_command_t cli_list[] = {
 
 unsigned int cli_entries = sizeof(cli_list) / sizeof(cli_command);
 
+#define FUNCTION_ENTRY  \
+    do {    \
+        printf("%s:entry\n",__FUNCTION__);  \
+    } while(0);
+
+#define FUNCTION_EXIT   \
+    do {    \
+        printf("%s:exit\n",__FUNCTION__);   \
+    } while(0);
+
 /*
  * Commands
  */
 int foo(int *res)
 {
     *res = 3;
+    FUNCTION_ENTRY
+    FUNCTION_EXIT
     return 1;
 }
 
 int bar(int *res)
 {
     *res = 2;
+    FUNCTION_ENTRY
+    FUNCTION_EXIT
     return 0;
 }
 
 int barfoo(int *res)
 {
     *res = 5;
+    FUNCTION_ENTRY
+    FUNCTION_EXIT
     return 3;
 }
 
 int foobar(int *res)
 {
     *res = 6;
+    FUNCTION_ENTRY
+    FUNCTION_EXIT
     return 2;
 }
